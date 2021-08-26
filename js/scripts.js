9714a408ea9d024f86be6768ba0485d027776264
Sunshinepunch <78851729+Sunshinepunch@users.noreply.github.com>
2021-08-26 11:02:05 -0700

    works as intended

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,70 @@
+
 function Player(total,current) {
   this.total = total;
   this.current = current;
 }
 
 Player.prototype.roll = function(result) {
-  this.current += result;
+  if(result == 1) {
+    this.current = 0;
+    Player.prototype.hold(pOneScore.current);
+    alert("You rolled a 1! GET OUT!");
+  } else {
+    this.current += result;
+  }
 }
 
 function rollDice(min, max) {
-  return (min-1) + Math.ceil(Math.random() * (max-min + 1))
-  ;
+  if(pOneScore.total >=  100) {
+    alert("Player One Wins! Player Two sucks eggs!");
+  } else if (pTwoScore.total >= 100) {
+    alert("Player Two Wins! Player One uses paper towels for tissue.");
+  } else {
+  return (min-1) + Math.ceil(Math.random() * (max-min + 1));
+  }
 }
 
 Player.prototype.hold = function(current) {
-  this.total += current;
-  this.current = 0;
+  if(pOneScore.total >= 100) {
+    alert('Wow you won. Go outside.');
+  } else if (pTwoScore.total >= 100) {
+      alert('Huh. Guess your dad was wrong. You can do something.');
+  } else {
+    this.total += current;
+    this.current = 0;
+  }
 }
 
 
 
-
-//if number is not 1, add total to currentPoints
-//if dice rolls 1, pop alert and hide current players button and
-//shows the other players button
-//display current total somewhere
-
-//display P1 total somewhere
-//display P2 total somewhere
-
-//display number rolled
-
-//UI logic
 
 
 let pOneScore = new Player (0,0);
 let results = 0
 let pTwoScore = new Player (0,0);
 
+function AHHHH() {
+  if (results===1) {
+    $('#play-area-1').toggle();
+    $('#play-area-2').toggle();
+  };
+}
+
+
+
 $(document).ready(function() {
   $("form#roll-dice1").submit(function(event) {
     event.preventDefault();
     results = rollDice(1,6);
     pOneScore.roll(results);
-    console.log(pOneScore.current);
+    AHHHH(results);
     $("#chalkboard1").text(pOneScore.current);
   });
 
   $("form#hold-one").submit(function(event) {
     event.preventDefault();
     pOneScore.hold(pOneScore.current);
-    console.log(pOneScore.total);
+    $("#totalscore1").text(pOneScore.total);
     $("#play-area-2").show();
     $("#play-area-1").hide();
   });
@@ -59,14 +74,14 @@ $(document).ready(function() {
     event.preventDefault();
     results= rollDice(1,6);
     pTwoScore.roll(results);
-    console.log(pTwoScore.current);
+    AHHHH(results);
     $("#chalkboard2").text(pTwoScore.current);
   });
 
   $("form#hold-two").submit(function(event) {
     event.preventDefault();
     pTwoScore.hold(pTwoScore.current);
-    console.log(pTwoScore.total);
+    $("#totalscore2").text(pTwoScore.total);
     $("#play-area-2").hide();
     $("#play-area-1").show();
   });
